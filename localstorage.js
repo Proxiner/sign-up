2ccd961d3d03e1c29aa7ef2c8df7aa2e7b541b52
Taha <tahaemamy107@gmail.com>
2023-08-12 19:42:15 +0330

    ggggggggggggkthg

---
+++ localstorage.js
@@ -0,0 +1,12 @@
+function signUp(event) {
+
+    event.preventDefault();
+  
+    let fullname = document.getElementById("fullname").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+  
+        localStorage.setItem(`fullname` , `${fullname}`)
+        localStorage.setItem(`email` , `${email}`)
+        localStorage.setItem(`password` , `${password}`)
+  }
